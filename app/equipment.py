33c037b0d58b89b1dc1f7c6edeaa41e7ee80a433
sergd <videomc1979@gmail.com>
2023-02-23 22:22:55 +0300

    ready equipment.py

--- app/equipment.py
+++ app/equipment.py
@@ -8,64 +8,80 @@ import json
 
 @dataclass
 class Armor:
+    id: int
     name: str
-    defence: int
+    defence: float
     stamina_per_turn: int  # выносливость за ход
 
 
 @dataclass
 class Weapon:
+    id: int
     name: str
     min_damage: float
     max_damage: float
     stamina_per_hit: int  # выносливость за удар
 
     @property
-    def damage(self):
-        pass
+    def damage(self) -> float:
+        return uniform(self.min_damage, self.max_damage)
 
 
 @dataclass
 class EquipmentData:
-    #  TODO содержит 2 списка - с оружием и с броней
-    pass
+    #  содержит 2 списка - с оружием и с броней
+    armors: List[Armor]
+    weapons: List[Weapon]
 
 
 class Equipment:
-
     def __init__(self):
         self.equipment = self._get_equipment_data()
 
+    def get_weapon(self, weapon_name) -> Weapon:
+        # возвращает объект оружия по имени
+        for weapon in self.get_weapons_names():
+            if weapon.name == weapon_name:
+            # if weapon_name == self.equipment['name']:
+                return weapon
+        raise NotImplementedError
+
+    def get_armor(self, armor_name) -> Armor:
+        # возвращает объект брони по имени
+        for armor in self.get_armors_names():
+            if armor.name == armor_name:
+                return armor
+        raise NotImplementedError
+
+    def get_weapons_names(self) -> list:
+        # возвращаем список с оружием
+        return [item.name for item in self.equipment[0]]
+
+    def get_armors_names(self) -> list:
+        # возвращаем список с броней
+        return [item.name for item in self.armor]
+
     @staticmethod
     def _get_equipment_data() -> EquipmentData:
-        #  TODO этот метод загружает json в переменную EquipmentData
-        with open("../data/equipment.json", "r", encoding="utf-8") as file:
-        # equipment_file = open()
-            data = json.load(file)
-            print(data)
-            equipment_schema = marshmallow_dataclass.class_schema(data)
-            print(equipment_schema)
+        # этот метод загружает json в переменную EquipmentData
+        equipment_file = open("../data/equipment.json", encoding="utf-8")
+        data = json.load(equipment_file)
+        # print(data['weapons'][0]['name'])
+        equipment_schema = marshmallow_dataclass.class_schema(EquipmentData)
+        # print(equipment_schema().dump(data)['weapons'])
         try:
+            # return equipment_schema().dump(data)
             return equipment_schema().load(data)
         except marshmallow.exceptions.ValidationError:
             raise ValueError
 
-    def get_weapon(self, weapon_name) -> Weapon:
-        pass
 
-    def get_armor(self, armor_name) -> Armor:
-        #  TODO возвращает объект брони по имени
-        pass
+# d = Equipment()
+# print(d.equipment['weapons'])
+# print(d.equipment['armors'])
+# print(d.equipment.weapons[0])
+# print(d.equipment.armors[0])
 
-    def get_weapons_names(self) -> list:
-        #  TODO возвращаем список с оружием
-        pass
-
-    def get_armors_names(self) -> list:
-        #  TODO возвращаем список с броней
-        pass
-
-
-
-d = Equipment()
-print(d.get_weapon())
+# s = EquipmentData(Armor, Weapon)
+# print(s.armors)
+# print(s.weapons)
